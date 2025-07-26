d83c003295cf45645324520500a8baad1fe920ba
lucastefanescu <lucastefanescu383@gmail.com>
2025-07-25 22:41:08 -0400

    abcd

--- volt-ev/frontend/src/features/Admin/AddVehicle.jsx
+++ volt-ev/frontend/src/features/Admin/AddVehicle.jsx
@@ -1,10 +1,15 @@
 import { useDropzone } from "react-dropzone";
+<<<<<<< HEAD
 import { useState, useEffect, useRef, useMemo } from "react";
+=======
+import { useState, useEffect } from "react";
+>>>>>>> 9cbf8dbac4d4600256eab4e2e5cacd963708b4f1
 import axios from "axios";
 import "./addvehicles.css";
 
 function AddVehicle() {
 	const [files, setFiles] = useState([]);
+<<<<<<< HEAD
 	const fileRef = useRef(null);
 
 	const baseStyle = {
@@ -19,15 +24,33 @@ function AddVehicle() {
 	const focusedStyle = {};
 	const acceptStyle = {};
 	const rejectStyle = {};
+=======
+
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: { "image/*": [] },
+		onDrop: (acceptedFiles) => {
+			setFiles(
+				acceptedFiles.map((obj) => {
+					return Object.assign(obj, { preview: URL.createObjectURL(obj) });
+				})
+			);
+		},
+	});
+>>>>>>> 9cbf8dbac4d4600256eab4e2e5cacd963708b4f1
 
 	useEffect(() => {
 		return () => {
 			files.forEach((element) => {
+<<<<<<< HEAD
 				URL.revokeObjectURL(element);
+=======
+				URL.removeObjectURL(element);
+>>>>>>> 9cbf8dbac4d4600256eab4e2e5cacd963708b4f1
 			});
 		};
 	}, [files]);
 
+<<<<<<< HEAD
 	const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
 		useDropzone({
 			accept: { "image/*": [] },
@@ -63,6 +86,41 @@ function AddVehicle() {
 		console.log(formData);
 		axios
 			.post("http://localhost:8080/addvehicle/upload", formData)
+=======
+	function handleFormSubmit(e) {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+
+		const formValues = {
+			image: formData.get("image"),
+			price: formData.get("price"),
+			colour: formData.get("colour"),
+			drivetrain: formData.get("drivetrain"),
+			year: formData.get("year"),
+			range: formData.get("range"),
+			horsepower: formData.get("horsepower"),
+			acceleration: formData.get("acceleration"),
+			battery: formData.get("battery"),
+			charging: formData.get("charging"),
+			efficiency: formData.get("efficiency"),
+		};
+		console.log(formValues);
+
+		axios
+			.post("http://localhost:8080/addvehicle/", {
+				image: formData.get("image"),
+				price: formData.get("price"),
+				colour: formData.get("colour"),
+				drivetrain: formData.get("drivetrain"),
+				year: formData.get("year"),
+				range: formData.get("range"),
+				horsepower: formData.get("horsepower"),
+				acceleration: formData.get("acceleration"),
+				battery: formData.get("battery"),
+				charging: formData.get("charging"),
+				efficiency: formData.get("efficiency"),
+			})
+>>>>>>> 9cbf8dbac4d4600256eab4e2e5cacd963708b4f1
 			.then((response) => {
 				console.log(response);
 			})
@@ -70,6 +128,7 @@ function AddVehicle() {
 				console.log(err);
 			});
 	}
+<<<<<<< HEAD
 	return (
 		<div className="add-vehicles-container">
 			<div className="add-vehicles-form-wrapper">
@@ -202,6 +261,61 @@ function AddVehicle() {
 				</form>
 			</div>
 		</div>
+=======
+	function handleButtonClick() {
+		axios.get("http://");
+	}
+	return (
+		<form className="add-vehicle-form" onSubmit={handleFormSubmit}>
+			<div {...getRootProps({ className: "dropzone" })}>
+				<label for="image"></label>
+				<input id="image" name="image" {...getInputProps()} />
+				{files.map((file) => (
+					<img
+						src={file.preview}
+						onLoad={() => URL.revokeObjectURL(file)}
+						alt="car"
+					/>
+				))}
+			</div>
+			<label for="price">Price</label>
+			<input type="text" id="price" name="price" />
+			<label for="colour">Colour</label>
+			<select id="color" name="colour">
+				<option value="white">White</option>
+				<option value="black">Black</option>
+				<option value="navy">Navy</option>
+				<option value="beige">Beige</option>
+				<option value="orange">Orange</option>
+				<option value="gray">Gray</option>
+				<option value="green">Green</option>
+				<option value="blue">Blue</option>
+			</select>
+			<label for="drivetrain">Drivetrain</label>
+			<select id="drivetrain" name="drivetrain">
+				<option value="AWD">AWD</option>
+				<option value="RWD">AWD</option>
+				<option value="FWD">AWD</option>
+			</select>
+			<label for="year">Year</label>
+			<input id="year" name="year" />
+			<label for="range">Range</label>
+			<input id="range" name="range" />
+			<label for="horsepower">Horsepower</label>
+			<input id="horsepower" name="horespower" />
+			<label for="acceleration">Acceleration</label>
+			<input id="acceleration" name="acceleration" />
+			<label for="battery">Battery</label>
+			<input id="battery" name="battery" />
+			<label for="charging">Charging</label>
+			<input id="charging" name="charging" />
+			<label for="efficiency">Efficiency</label>
+			<input id="efficiency" name="efficiency" />
+
+			<button type="submit">Add Vehicle</button>
+			<button onClick={handleButtonClick}></button>
+		</form>
+>>>>>>> 9cbf8dbac4d4600256eab4e2e5cacd963708b4f1
 	);
 }
 export default AddVehicle;
